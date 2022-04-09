b689cf0d86d7867029f68dc6719ba6fbef708030
HealyTea12 <37420604+HealyTea12@users.noreply.github.com>
2022-04-09 20:05:45 +0200

    Add files via upload

---
+++ covidproject_1.sql
@@ -0,0 +1,50 @@
+-- checking that the data is there
+-- select * from covid..coviddeaths;
+-- select * from covid..covidvaccines order by location;
+
+
+-- percent infected, percent killed and percent infected that died.
+
+
+select location,total_cases, total_deaths, (total_deaths/total_cases)*100 as mortality_rate,
+population,(total_cases/population)*100 as infection_perc,
+(total_deaths/population)*100 as death_perc
+from covid..coviddeaths
+where location like 'Germ%'
+order by location, date;
+
+-- countries with largest infection rate
+
+select location, max(total_cases/population)*100 as infection_perc
+from covid..coviddeaths
+group by location
+order by infection_perc desc;
+
+-- countries with largest death rate
+
+select location, max(total_deaths/population)*100 as death_perc
+from covid..coviddeaths
+group by location
+order by death_perc desc;
+
+-- world
+
+select DATEPART(year, date) as year_, DATEPART(week, date) as week_,
+sum(cast(new_deaths as int)) as total_new_deaths, sum(cast(new_cases as int)) as total_new_cases
+from covid..coviddeaths
+where continent is not null
+group by DATEPART(year, date), DATEPART(week, date)
+order by DATEPART(year, date), DATEPART(week, date);
+
+
+-- joining the two tables 
+
+with deaths_vaccines as 
+(
+select * 
+from covid..coviddeaths as d
+inner join covid..covidvaccines as v
+	on d.location = v.location
+	and d.date = v.date
+)
+select * from deaths_vaccines;
